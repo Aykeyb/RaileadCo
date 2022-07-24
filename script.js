fe83c8fb9ed41b66e43c68fb55959efd090b1b67
Aykeyb <108504115+Aykeyb@users.noreply.github.com>
2022-07-24 21:15:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+
+    var typed = new Typed(".typing", {
+        strings: ["Malaysian Heroes", "New Wave", "Aykeyb Dream", "Revolution", "Legit", "Mission", "Vision"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Lifestyle", "Style", "Leader", "Design", "Vision", "Mission"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
